a393c64e159c06de2349271b71338c6840f66178
wiwo-one <wojtekwieclawski@gmail.com>
2022-11-18 18:25:10 +0530

    simplified context provider

--- components/ShoppingCart/ECommerceContext.jsx
+++ components/ShoppingCart/ECommerceContext.jsx
@@ -1,5 +1,94 @@
-import { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 export const ECommerceContext = createContext();
 
-export const ECommerceProvider = ECommerceContext.Provider;
+//export const ECommerceProvider = ECommerceContext.Provider;
+
+export function ECommerceProvider({ children }) {
+  const [productsCount, setProductsCount] = useState(0);
+  const [cartItems, setCartItems] = useState([]);
+  //const [cartItemsGrouped, setCartItemsGrouped] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [order, setOrder] = useState({});
+
+  useEffect(() => {
+    //set total number of products
+    setProductsCount(cartItems.reduce((acc, item) => acc + item.quantity, 0));
+    //set total price
+    setTotalPrice(
+      cartItems.reduce((acc, item) => {
+        return acc + item.product.price * item.quantity;
+      }, 0)
+    );
+  }, [cartItems]);
+
+  useEffect(() => {
+    if (cartItems.length > 0) {
+      localStorage.setItem("cartItems", JSON.stringify(cartItems));
+    }
+  }, [cartItems]);
+
+  useEffect(() => {
+    const local = localStorage.getItem("cartItems");
+    if (local) {
+      setCartItems(JSON.parse(local));
+    }
+  }, []);
+
+  const addProduct = ({ product }) => {
+    const cartItemsCopy = [...cartItems];
+    //find index of given product in cartItems
+    const productIndex = cartItemsCopy.findIndex((item) => item.productId === product.id);
+    if (productIndex >= 0) {
+      cartItemsCopy[productIndex].quantity++;
+      setCartItems([...cartItemsCopy]);
+    } else {
+      cartItemsCopy.push({
+        productId: product.id,
+        quantity: 1,
+        product: product,
+      });
+      setCartItems([...cartItemsCopy]);
+    }
+  };
+
+  const removeProduct = ({ product }) => {
+    const cartItemsCopy = [...cartItems];
+    //find index of given product in cartItems
+    const productIndex = cartItemsCopy.findIndex((item) => item.productId === product.id);
+
+    if (productIndex >= 0) {
+      if (cartItemsCopy[productIndex].quantity > 1) {
+        cartItemsCopy[productIndex].quantity--;
+        setCartItems([...cartItemsCopy]);
+      } else {
+        setCartItems(cartItemsCopy.filter((ci) => ci.productId !== product.id));
+      }
+    } else {
+      console.log("THIS PRODUCT IS NOT IN THE CART");
+    }
+  };
+
+  const removeAllProducts = ({ product }) => {
+    const filtered = cartItems.filter((ci) => ci.productId !== product.id);
+    setCartItems([...filtered]);
+  };
+
+  const emptyCart = () => {
+    setCartItems([]);
+    localStorage.removeItem("cartItems");
+  };
+
+  const value = {
+    cartItems,
+    productsCount,
+    totalPrice,
+    addProduct,
+    removeProduct,
+    removeAllProducts,
+    emptyCart,
+    order,
+    setOrder,
+  };
+  return <ECommerceContext.Provider value={value}>{children}</ECommerceContext.Provider>;
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,16 @@
 import "styles/globals.css";
 import { SessionProvider } from "next-auth/react";
-//import useShoppingCart from "components/ShoppingCart/useShoppingCart";
 
 import { ECommerceProvider } from "components/ShoppingCart/ECommerceContext";
 import { AnimatePresence } from "framer-motion";
-import useECommerce from "components/ShoppingCart/useECommerce";
 
 function MyApp({ Component, pageProps: { session, ...pageProps }, router }) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page);
 
-  const url = `https://veggiesandfruits.com/${router.route}`;
-
-  const eCommerceProps = useECommerce();
-
   return (
     <SessionProvider session={session}>
-      <ECommerceProvider value={eCommerceProps}>{getLayout(<Component {...pageProps} />)}</ECommerceProvider>
+      <ECommerceProvider>{getLayout(<Component {...pageProps} />)}</ECommerceProvider>
     </SessionProvider>
   );
 }
