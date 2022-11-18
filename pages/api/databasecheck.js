abd9e02c67f98085e9cc482ba193480d3e091e1f
wiwo-one <wojtekwieclawski@gmail.com>
2022-11-18 23:12:01 +0530

    database check

--- pages/api/databasecheck.js
+++ pages/api/databasecheck.js
@@ -2,18 +2,18 @@ import prisma from "lib/prisma";
 
 // to login the action
 prisma.$use(async (params, next) => {
-  // const before = Date.now();
-  // const result = await next(params);
-  // const after = Date.now();
-  // console.log(`Query ${params.model}.${params.action} took ${after - before}ms`);
-  // return result;
+  const before = Date.now();
+  const result = await next(params);
+  const after = Date.now();
+  console.log(`Query ${params.model}.${params.action} took ${after - before}ms`);
+  return result;
 });
 
 export default async function handler(req, res) {
   const data = { note: `Chekup done on: ${new Date().toLocaleString("pl-PL")}` };
   try {
     const databaseCheck = await prisma.databaseCheck.create({ data });
-    console.log(`Added checkup: ${databaseCheck}`);
+    console.log(`Added checkup: ${JSON.stringify(databaseCheck, null, 2)}`);
     res.status(200).json(databaseCheck);
   } catch (error) {
     console.error(error);
