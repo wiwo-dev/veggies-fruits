d34bd4878dbb998569a386cfed00403eb4a9473b
wiwo-one <wojtekwieclawski@gmail.com>
2022-12-08 20:22:14 +0530

    file names changes

--- components/admin/Sidebar2.js
+++
@@ -1,6 +0,0 @@
-import React, { useState } from "react";
-
-export default function Sidebar2() {
-  const [isOpen, setIsOpen] = useState(false);
-  return <div className="fixed left-0 top-[40px] h-screen min-w-[200px] bg-primary-9">Sidebar2</div>;
-}
